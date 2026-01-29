257e742f16db823bc10cbbc7d858eea4bbc55e52
hermangavrylov <gavrylovherman@gmail.com>
2026-01-29 22:53:50 +0100

    Implement header to all pages. Create nav-bar

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {page: 'Home'});
 });
 
 app.get("/blogs", (req, res) => {
-    res.render("blogs.ejs");
+    res.render("blogs.ejs", {page: "Blogs"});
 });
 
 app.get("/create", (req, res) => {
-    res.render("create.ejs");
+    res.render("create.ejs", {page: "Create"});
 });
 
 app.listen(port, () => {
