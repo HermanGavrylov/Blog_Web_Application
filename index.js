dc745944418da4e192f1976ebbd8e8bbaa525f2a
hermangavrylov <gavrylovherman@gmail.com>
2026-01-29 21:54:29 +0100

    features: add blogs and create page routes

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/blogs", (req, res) => {
+    res.render("blogs.ejs");
+});
+
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
