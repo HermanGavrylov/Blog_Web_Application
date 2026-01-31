cc80ed29f74495e1391481512b49bbc271335096
hermangavrylov <gavrylovherman@gmail.com>
2026-01-31 23:53:16 +0100

    Make Minor CSS Changes

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {page: 'Home'});
+  res.render("index.ejs", { page: "Home" });
 });
 
 app.get("/blogs", (req, res) => {
-    res.render("blogs.ejs", {page: "Blogs"});
+  res.render("blogs.ejs", { page: "Blogs" });
 });
 
 app.get("/create", (req, res) => {
-    res.render("create.ejs", {page: "Create"});
+  res.render("create.ejs", { page: "Create" });
 });
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
